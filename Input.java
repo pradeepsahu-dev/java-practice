7e122f02dab724a5ba10cb5368365c978582328a
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-13 23:56:20 +0530

    start practice questions

---
+++ Input.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Input{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+     
+        //  sum of A&B.
+
+        //  int a = sc.nextInt();
+        //  int b = sc.nextInt();
+
+        //  int Sum = a+b;
+        //  System.out.print("This is Sum or a&b:" + Sum);
+
+        //  Prodect of A&B.
+        //  int A = sc.nextInt();
+        //  int B = sc.nextInt();
+        //  int sum = A*B;
+        //  System.out.print("this is product of A&B:" + sum);
+
+
+
+
+    //Area of circle
+
+    float rad = sc.nextFloat();
+
+    float area = 3.14f *rad*rad;
+    System.out.println(area);
+
+
+        
+    }
+}
