f16e01027aff8f63bf5af6e5ebc8dd64ce67e87b
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-16 01:12:15 +0530

    Assignment3

---
+++ Assignment3/Checkno.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Checkno {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int nums = sc.nextInt();
+
+        if(nums>0){
+            System.out.println("Positive number");
+
+        }else{
+            System.out.println("negative number");
+        }
+
+    }
+    
+}
