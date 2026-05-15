dbb90bc23897eadd5dce076555c49504781d79e3
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-16 01:28:56 +0530

    Switch problem

---
+++ Assignment3/Temperature.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Temperature {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Temperature");
+        double temp = sc.nextDouble();
+
+        if(temp >= 100){
+            System.out.println("You have Fever");
+
+        }else{
+            System.out.println("You Have No fever");
+        }
+
+        
+    }
+    
+}
