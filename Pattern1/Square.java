d65c5368b7fdbcc05737f5f93a4d98fb6f6c589c
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-17 15:58:08 +0530

    star patter

---
+++ Pattern1/Square.java
@@ -0,0 +1,9 @@
+// print Square pattern
+
+public class Square {
+    public static void main(String[]args){
+        for(int i=1; i<=5; i++){
+            System.out.println("* * * *");
+        }
+    }
+}
---
+++ Pattern1/Star.java
@@ -0,0 +1,11 @@
+public class Star{
+    public static void main(String[] args){
+        // print star pattern 
+        for(int line=1; line<=5; line++){
+            for(int star=1; star<=line; star++){
+                System.out.print("*");
+            }
+            System.err.println();
+        }
+    }
+}
