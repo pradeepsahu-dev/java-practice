2c9d9dda57c476f4c53947f13d486e582347a4f4
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-18 23:26:24 +0530

    Binary to decimal

---
+++ Binary_to_Decimal.java
@@ -0,0 +1,24 @@
+public class Binary_to_Decimal {
+
+    public static void binaryTodecimal(int binNum){
+        int binary = binNum;
+        int pow = 0;
+        int decnum = 0;
+
+        while(binNum > 0){
+            int lastDigit = binNum % 10;
+            decnum = decnum + (lastDigit* (int)Math.pow(2,pow));
+
+            pow++;
+            binNum = binNum/10;
+        }
+
+        System.out.println("Decimal of "+binary+ "="+ decnum );
+
+    }
+    public static void main(String[]args){
+        binaryTodecimal(101);
+
+    }
+    
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,29 +1,57 @@
 import java.util.Scanner;
 
 public class PrimeNumber {
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number");
-        int n = sc.nextInt();
+
+    public static boolean isPrime(int n){
 
         if(n==2){
-            System.out.println("n is prime");
-        }else{
+            return true;
+        }
 
-             boolean isPrime = true;
-             for(int i=2; i<=n-1; i++){
-                if(n %i ==0){  // n is a multiple of i(i not equal to  1 or n)
-                 isPrime= false;
-                }
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i == 0){
+                return false;
             }
+        }
+        return true;
 
-            if(isPrime == true){
-            System.out.println("n is prime");
 
-            }else{
-            System.out.println("n is not a prime number");
-            }      
-        }   
+    }
+
+    public static void primeInRange(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                System.out.print(i+ " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+         primeInRange(20);
+
+        // System.out.print("Enter a number");
+        // int n = sc.nextInt();
+
+        // if(n==2){
+        //     System.out.println("n is prime");
+        // }else{
+
+        //      boolean isPrime = true;
+        //      for(int i=2; i<=n-1; i++){
+        //         if(n %i ==0){  // n is a multiple of i(i not equal to  1 or n)
+        //          isPrime= false;
+        //         }
+        //     }
+
+        //     if(isPrime == true){
+        //     System.out.println("n is prime");
+
+        //     }else{
+        //     System.out.println("n is not a prime number");
+        //     }      
+        // }   
     }
     
 }
