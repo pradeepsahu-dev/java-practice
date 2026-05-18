f32b3a6e24137e58f516faaf185787d4f50ecbf7
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-19 03:03:21 +0530

    check palindrome number

--- Assignment5/Test3.java
+++ Assignment5/Test3.java
@@ -1,11 +1,25 @@
-
-
 public class Test3 {
+    public static boolean Palindrom(int n){
+        int original = n;
+        int reverse = 0;
+
+        while(n>0){
+            int rem = n%10;
+            reverse = reverse*10+rem;
+            n = n/10;  
+        }
+        return reverse == original;
+
+       
+    }
     public static void main(String[]args){
         
-        int n =5;
-        System.out.println(n);
-
+        if(Palindrom(232)){
+             System.out.println("yes this is palindrome");
+         }else{
+            System.out.println("not palindrome");
+         }
+        
     }
 
     
