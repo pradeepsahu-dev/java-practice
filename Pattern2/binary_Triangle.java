f5f8b1e3c50679646cd0d2f0a4ce263ca65b4498
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-22 01:13:47 +0530

    0 and 1 triangle

---
+++ Pattern2/binary_Triangle.java
@@ -0,0 +1,22 @@
+public class binary_Triangle {
+
+    public static void Binary_triangle(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j) %2 ==0){
+                    System.out.print(1+" ");
+
+                }else{
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[]args){
+        Binary_triangle(5);
+
+    }
+    
+}
