d88917ad0988d2d40d4d22f26500085c4db3f176
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-23 20:53:34 +0530

    Solid Rhombus pattern

--- Pattern2/solid_Rhombus.java
+++ Pattern2/solid_Rhombus.java
@@ -5,15 +5,19 @@ public class solid_Rhombus {
         //outer
         for(int i=1; i<=n; i++){
             //inner
-            for(int j=1; j<=n; j++){
-                
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" "+" ");
             }
+            for(int j=1; j<=n; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
         }
 
     }
 
     public static void main(String[] args){
-        System.out.println("");
+        Solid_Rhombus(5);
     }
     
 }
