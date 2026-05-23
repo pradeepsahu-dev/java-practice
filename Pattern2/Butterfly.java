8e665f072c025c253af47e5958b80156bc110a9a
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-23 19:40:41 +0530

    Butterfly pattern

--- Pattern2/Butterfly.java
+++ Pattern2/Butterfly.java
@@ -1,5 +1,7 @@
 public class Butterfly {
     public static void butterfly(int n){
+
+        //first half
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
                 // star
@@ -12,6 +14,25 @@ public class Butterfly {
 
             }
 
+            //star
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+        // second half
+
+        for(int i=n; i>=1; i--){
+            //star
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+
+            //space
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" "+" ");
+            }
             //star
             for(int j=1; j<=i; j++){
                 System.out.print("*"+" ");
