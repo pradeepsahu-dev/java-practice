70a1a48a1c63c69dbd71ecc982001d9a0cd0a0c8
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-23 20:26:35 +0530

    solid rhombus problem

---
+++ Pattern2/solid_Rhombus.java
@@ -0,0 +1,19 @@
+public class solid_Rhombus {
+
+    public static void Solid_Rhombus(int n){
+
+        //outer
+        for(int i=1; i<=n; i++){
+            //inner
+            for(int j=1; j<=n; j++){
+                
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("");
+    }
+    
+}
