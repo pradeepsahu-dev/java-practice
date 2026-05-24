ce66dddba9f9cb4ac7bb4bf7d2c8c83643627759
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-24 23:54:53 +0530

    diamond problem

---
+++ Pattern2/Diamond.java
@@ -0,0 +1,13 @@
+public class Diamond {
+
+    public static void diamond(int n){
+        for(int i=1; i<=n; i++){
+            
+        }
+    }
+
+    public static void main(String[]args){
+
+    }
+    
+}
