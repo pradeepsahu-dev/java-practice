f4e628f0d022112f6d23f7beeeb5d51551acf4bb
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-24 22:42:01 +0530

    hollow rhombus

---
+++ Pattern2/Hollow_Rhombus.java
@@ -0,0 +1,13 @@
+public class Hollow_Rhombus {
+
+    public static void hollow_RHombus(int n){
+        for(int i=1; i<=n; i++){
+            
+        }
+    }
+
+    public static void main(String[]args){
+
+    }
+    
+}
