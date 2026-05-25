41ecc72a886717c3256f7dc85c2388e244bc3b4f
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-25 21:51:56 +0530

    Diamond Pattern

--- Pattern2/Diamond.java
+++ Pattern2/Diamond.java
@@ -1,23 +1,38 @@
 public class Diamond {
 
     public static void diamond(int n){
+        // first half
         for(int i=1; i<=n; i++){
             // spaces
             for(int j=1; j<=n-i; j++){
-                System.out.println(" "+" ");
+                System.out.print(" "+" ");
 
             }
             // star
             for(int j=1; j<=(2*i)-1; j++){
-                System.out.println("*"+" ");
+                System.out.print("*"+" ");
 
             }
+            System.out.println();
         
             
+        }
+
+        // second half
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" "+" ");
+
+            }
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[]args){
+        diamond(4);
 
     }
     
